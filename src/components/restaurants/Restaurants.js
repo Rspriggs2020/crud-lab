5358945a6276f9d7a143bcec9585e4cb47770999
ryliespriggs <rspriggs812@gmail.com>
2021-11-29 14:12:53 -0800

    commit tree 01a6274c7e901c2a69c8407d3ae9755bef1e79ea

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const {restaurants, deleteRestaurant} = this.props;
+    const res = restaurants.map(restaurant => {
     return(
+      <Restaurant
+      key={restaurant.id}
+      restaurant={restaurant}
+      deleteRestaurant={deleteRestaurant}
+      />
+    )
+    })
+    return (
       <ul>
-        Restaurants Component
+        {res}
       </ul>
-    );
+    )
   }
-};
+}
 
 export default Restaurants;
